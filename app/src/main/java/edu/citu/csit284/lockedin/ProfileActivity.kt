6526c7c05d76bfd68781888ee99cd1b4ad701d51
Clorky <clarkisnoob434@gmail.com>
2025-02-18 23:53:30 +0800

    Made a function for the pop-up prompt

--- app/src/main/java/edu/citu/csit284/lockedin/ProfileActivity.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/ProfileActivity.kt
@@ -14,22 +14,31 @@ class ProfileActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val btn_back = findViewById<ImageButton>(R.id.button_back)
-        btn_back.setOnClickListener { startActivity(Intent(this, LandingActivity::class.java)) }
-
-        val btn_settings = findViewById<ImageButton>(R.id.button_settings)
-        btn_settings.setOnClickListener { startActivity(Intent(this, SettingsActivity::class.java)) }
 
         val tvName = findViewById<TextView>(R.id.tv_name)
         val btnEditName = findViewById<ImageButton>(R.id.button_edit_name)
-        btnEditName.setOnClickListener {
-            val inputName = EditText(this)
-            inputName.setText(tvName.text.toString())
+
+        val tvUsername = findViewById<TextView>(R.id.tv_username)
+        val btnEditUsername = findViewById<ImageButton>(R.id.button_edit_username)
+
+        val tvEmail = findViewById<TextView>(R.id.tv_email)
+        val btnEditEmail = findViewById<ImageButton>(R.id.button_edit_email)
+
+        setUpEditButton(btnEditName, tvName, "Enter your new name", "Full Name")
+        setUpEditButton(btnEditUsername, tvUsername, "Enter your new username", "Username")
+        setUpEditButton(btnEditEmail, tvEmail, "Enter your new email", "Email Address")
+    }
+
+    fun setUpEditButton(editButton: ImageButton, textView: TextView, title: String, hintText: String ) {
+        editButton.setOnClickListener {
+            val inputField = EditText(this)
+            inputField.setText(textView.text.toString())
+            inputField.hint = hintText
 
             val dialog = AlertDialog.Builder(this)
-                .setTitle("Enter your new name")
-                .setView(inputName)
-                .setPositiveButton("OK") { _, _ -> tvName.text = inputName.text.toString() }
+                .setTitle(title)
+                .setView(inputField)
+                .setPositiveButton("OK") { _, _ -> textView.text = inputField.text.toString() }
                 .setNegativeButton("Cancel", null)
                 .create()
             dialog.show()
