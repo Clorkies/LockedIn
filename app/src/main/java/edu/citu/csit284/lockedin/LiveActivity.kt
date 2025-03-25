f4d95db929349406089629839ba8ddf9de209111
Clorkies <clarkisnoob434@gmail.com>
2025-03-25 23:23:37 +0800

    Integrated screen navigation distinction for the rest of the pages.

--- app/src/main/java/edu/citu/csit284/lockedin/LiveActivity.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/LiveActivity.kt
@@ -10,8 +10,34 @@ class LiveActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live)
 
+        val btn_landing = findViewById<ImageButton>(R.id.button_home)
+        btn_landing.setOnClickListener {startActivity(Intent(this, LandingActivity::class.java)) }
+
+        val btn_profile = findViewById<ImageButton>(R.id.button_profile)
+        btn_profile.setOnClickListener {startActivity(Intent(this, ProfileActivity::class.java)).apply {
+            intent.putExtra("caller", "live")
+        } }
+
+        val btn_games = findViewById<ImageButton>(R.id.button_games)
+        btn_games.setOnClickListener {startActivity(Intent(this, GamesActivity::class.java)) }
+
+        val btn_live = findViewById<ImageButton>(R.id.button_live)
+        btn_live.setOnClickListener {startActivity(Intent(this, LiveActivity::class.java)) }
+
+        val btn_explore = findViewById<ImageButton>(R.id.button_explore)
+        btn_explore.setOnClickListener {startActivity(Intent(this, ExploreActivity::class.java)) }
+
+        val caller = intent.getStringExtra("caller")
 
         val btn_back = findViewById<ImageButton>(R.id.button_back)
-        btn_back.setOnClickListener {startActivity(Intent(this, LandingActivity::class.java)) }
+        btn_back.setOnClickListener {
+            when (caller) {
+                "landing" -> startActivity(Intent(this, LandingActivity::class.java))
+                "game" -> startActivity(Intent(this, GamesActivity::class.java))
+                "live" -> startActivity(Intent(this, LiveActivity::class.java))
+                "explore" -> startActivity(Intent(this, ExploreActivity::class.java))
+                else -> finish()
+            }
+        }
     }
 }
