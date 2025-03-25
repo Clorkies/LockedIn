91124ff91baaa1927f47ac0ec0d7d63500232d8c
Clorkies <clarkisnoob434@gmail.com>
2025-03-25 22:19:12 +0800

    Implementing other functionalities.

--- app/src/main/java/edu/citu/csit284/lockedin/LandingActivity.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/LandingActivity.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ListView
+import edu.citu.csit284.lockedin.data.Article
+import edu.citu.csit284.lockedin.helper.ArticleCustomListView
 
 class LandingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +25,29 @@ class LandingActivity : Activity() {
         val btn_explore = findViewById<ImageButton>(R.id.button_explore)
         btn_explore.setOnClickListener {startActivity(Intent(this, ExploreActivity::class.java)) }
 
+        val listView = findViewById<ListView>(R.id.articleListView)
+        val articleList = listOf(
+            Article(R.drawable.img_sample, "Article title 1", getString(R.string.lorem_ipsum)),
+            Article(R.drawable.img_sample2, "Article title 2", getString(R.string.lorem_ipsum)),
+            Article(R.drawable.img_sample3, "Article title 3", getString(R.string.lorem_ipsum)),
+            Article(R.drawable.img_sample, "Article title 4", getString(R.string.lorem_ipsum)),
+            Article(R.drawable.img_sample2, "Article title 5", getString(R.string.lorem_ipsum)),
+            Article(R.drawable.img_sample3, "Article title 6", getString(R.string.lorem_ipsum))
+        )
+
+        listView.adapter = ArticleCustomListView(
+            this,
+            articleList,
+            onClick = {
+                startActivity (
+                    Intent(this, ExploreArticleActivity::class.java).apply {
+                        putExtra("imageResource", it.imgResId)
+                        putExtra("title", it.title)
+                        putExtra("articleText", it.articleText)
+                    }
+                )
+            }
+        )
 
     }
 }
