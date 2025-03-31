27f91b1cf00270272c92fb44138062a957e5cd09
jermochi <jrvnryle@gmail.com>
2025-03-31 21:32:07 +0800

    Improved devpage

---
+++ app/src/main/java/edu/citu/csit284/lockedin/tempdev.kt
@@ -0,0 +1,17 @@
+package edu.citu.csit284.lockedin
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+
+class tempdev : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_dev)
+
+        val btn_back = findViewById<ImageButton>(R.id.button_back)
+        btn_back.setOnClickListener { startActivity(Intent(this, SettingsActivity::class.java)) }
+
+    }
+}
