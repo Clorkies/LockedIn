a91673e14b78aaaa727414c59dbd3fc93df0dccb
Clorkies <clarkisnoob434@gmail.com>
2025-04-09 21:26:54 +0800

    Cleared unused imports.

--- app/src/main/java/edu/citu/csit284/lockedin/RegisterActivity.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/RegisterActivity.kt
@@ -3,7 +3,6 @@ package edu.citu.csit284.lockedin
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -12,11 +11,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import edu.citu.csit284.lockedin.util.toggle
 
 class RegisterActivity : Activity() {
@@ -45,7 +40,7 @@ class RegisterActivity : Activity() {
         btnRegister.isEnabled = false
         btnRegister.setBackgroundResource(R.drawable.btn_register_disabled)
         password.addTextChangedListener(object : TextWatcher {
-            @SuppressLint("ResourceAsColor")
+            @SuppressLint("ResourceAsColor", "SetTextI18n")
             override fun afterTextChanged(s: Editable?) {
                 val pass = s.toString()
                 var metRules = 0
