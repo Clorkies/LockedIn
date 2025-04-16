61599e6fa3b8bbf3fe23eb0f183a5243d3b4ed83
Clorkies <clarkisnoob434@gmail.com>
2025-04-16 19:08:40 +0800

    Added more details for the About Developers page

--- app/src/main/java/edu/citu/csit284/lockedin/activities/AboutDevActivity.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/activities/AboutDevActivity.kt
@@ -1,11 +1,18 @@
 package edu.citu.csit284.lockedin.activities
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import edu.citu.csit284.lockedin.R
@@ -31,6 +38,55 @@ class AboutDevActivity : Activity() {
         val origName = name.text
         val origHobby = hobby.text
         val origFact = fact.text
+
+        val bottomSheetContainer = findViewById<LinearLayout>(R.id.devsBottomSheet)
+        val moreInfoArrow = findViewById<ImageView>(R.id.moreInfoArrow)
+
+        bottomSheetContainer.translationY = 1900f
+        var isBottomSheetExpanded = false
+
+        var arrowOffset = 10f
+        var positionOffset = 0f
+
+        var arrowBounceAnimation = ValueAnimator.ofFloat(-arrowOffset + positionOffset - 10f, arrowOffset + positionOffset - 10f)
+        arrowBounceAnimation.duration = 800
+        arrowBounceAnimation.repeatCount = ValueAnimator.INFINITE
+        arrowBounceAnimation.repeatMode = ValueAnimator.REVERSE
+        arrowBounceAnimation.interpolator = LinearInterpolator()
+        arrowBounceAnimation.addUpdateListener { animation ->
+            val value = animation.animatedValue as Float
+            moreInfoArrow.translationY = value
+        }
+        arrowBounceAnimation.start()
+
+        moreInfoArrow.setOnClickListener {
+            val slideAnimation: ObjectAnimator
+            if (!isBottomSheetExpanded) {
+                slideAnimation = ObjectAnimator.ofFloat(bottomSheetContainer, "translationY", 1900f, 0f)
+                moreInfoArrow.scaleY = 1f
+                positionOffset -= 140f
+            } else {
+                slideAnimation = ObjectAnimator.ofFloat(bottomSheetContainer, "translationY", 0f, 1900f)
+                moreInfoArrow.scaleY = -1f
+                positionOffset += 140f
+            }
+
+            arrowBounceAnimation = ValueAnimator.ofFloat(-arrowOffset + positionOffset - 10f, arrowOffset + positionOffset - 10f)
+            arrowBounceAnimation.duration = 800
+            arrowBounceAnimation.repeatCount = ValueAnimator.INFINITE
+            arrowBounceAnimation.repeatMode = ValueAnimator.REVERSE
+            arrowBounceAnimation.interpolator = LinearInterpolator()
+            arrowBounceAnimation.addUpdateListener { animation ->
+                val value = animation.animatedValue as Float
+                moreInfoArrow.translationY = value
+            }
+            arrowBounceAnimation.start()
+
+            slideAnimation.duration = 1100
+            slideAnimation.interpolator = DecelerateInterpolator()
+            slideAnimation.start()
+            isBottomSheetExpanded = !isBottomSheetExpanded
+        }
 
         clarkImg.animate().scaleX(1.2f).scaleY(1.2f).setDuration(200).start()
 
