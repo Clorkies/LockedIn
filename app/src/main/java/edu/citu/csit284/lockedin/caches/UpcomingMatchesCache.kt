e74d45c31a16d5f5323f64e961b25fd56172f362
jermochi <jrvnryle@gmail.com>
2025-04-29 19:18:25 +0800

    first idea - store games in a singleton

---
+++ app/src/main/java/edu/citu/csit284/lockedin/caches/UpcomingMatchesCache.kt
@@ -0,0 +1,19 @@
+package edu.citu.csit284.lockedin.caches
+
+import edu.citu.csit284.lockedin.data.Match
+
+class UpcomingMatchesCache {
+    private val cache = mutableMapOf<String, List<Match>>()
+    fun getMatchesFor(game: String): List<Match>? {
+        return cache[game]
+    }
+    fun storeMatchesFor(game: String, matches: List<Match>) {
+        cache[game] = matches
+    }
+    fun clearGameCache(game: String) {
+        cache.remove(game)
+    }
+    fun clearAll() {
+        cache.clear()
+    }
+}
