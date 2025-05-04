cbb309578a4f8a853554dc1c3d32d1e59ef116e9
Clorky <clarkisnoob434@gmail.com>
2025-05-04 21:31:55 +0800

    Final fixes on Articles Caching.

--- app/src/main/java/edu/citu/csit284/lockedin/util/NewsApiService.kt
+++ app/src/main/java/edu/citu/csit284/lockedin/util/NewsApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 interface NewsApiService {
     @GET("v2/everything")
     fun getEsportsArticles(
-        @Query("q") query: String = "(esports OR \"competitive gaming\" OR \"video games\" OR gaming) -football -soccer -basketball -baseball -cricket -rugby -UFC -county -County -(track and field) -(Real madrid) -(real madrid) -(Real Madrid) -NHL -NFL -NBA -MLB -golf -tennis -olympics -sports",
+        @Query("q") query: String = "(esports OR \"competitive gaming\" OR \"video games\" OR gaming) -football -soccer -basketball -baseball -cricket -rugby -UFC -county -\"track and field\" -\"real madrid\"  -UFC -county -WWE -madrid -united -star -samsung -Madrid -track -FIFA -surf -surfing -NHL -NFL -NBA -MLB -golf -tennis -olympics -sports",
         @Query("excludeDomains") excludeDomains: String = "espn.com,sports.yahoo.com,cbssports.com,nbcsports.com,bbc.com,businessinsider.com,npr.org",
         @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY,
         @Query("language") language: String = "en",
